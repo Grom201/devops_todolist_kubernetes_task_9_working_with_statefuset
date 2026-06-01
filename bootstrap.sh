e8cd9ffce5b31320887316c6b666465541a9d89a
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-06-01 12:46:01 +0200

    review changes bootstrap and mysql-init

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,14 @@ set -e
 kind create cluster --config cluster.yml
 
 kubectl apply -f .infrastructure/namespace.yml
-
 kubectl apply -f .infrastructure/mysql-secret.yml
 kubectl apply -f .infrastructure/mysql-init.yml
-
 kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/statefulSet.yml
-
 kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/configMap.yml
-kubectl apply -f .infrastructure/pv.yml
-kubectl apply -f .infrastructure/pvc.yml
-
+kubectl apply -f configMap.yml
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
 kubectl apply -f .infrastructure/deployment.yml
-kubectl apply -f .infrastructure/nodeport.yml
-kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f nodeport.yml
+kubectl apply -f hpa.yml
