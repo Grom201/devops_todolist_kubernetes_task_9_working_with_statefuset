3cb5f4ef8e9b6c87d4aee30a64ab797fd55a9536
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-06-01 12:36:52 +0200

    make changes after rewiev

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,22 @@
- #!/bin/bash
+#!/bin/bash
+
+set -e
+
 kind create cluster --config cluster.yml
 
 kubectl apply -f .infrastructure/namespace.yml
 
-kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/configMap.yml
+kubectl apply -f .infrastructure/mysql-secret.yml
+kubectl apply -f .infrastructure/mysql-init.yml
+
 kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/statefulSet.yml
 
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/configMap.yml
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+
 kubectl apply -f .infrastructure/deployment.yml
+kubectl apply -f .infrastructure/nodeport.yml
+kubectl apply -f .infrastructure/hpa.yml
